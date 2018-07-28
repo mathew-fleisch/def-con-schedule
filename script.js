fd573f23ff0fe54bc0a217a7e1182c7c25196f1b
Mathew Fleisch <mathew.fleisch@gmail.com>
2018-07-28 16:57:53 -0700

    Add a fixed mobile menu

--- script.js
+++ script.js
@@ -1,46 +1,58 @@
 jQuery(document).ready(function($) {
-	$(document).on('click', '.days_filter li', function() {
-		// console.log($(this).attr('id'))
-		const selectedDay = $(this).attr('id').replace(/day_filter_/, '')
-		if (selectedDay.match(/all/)) {
-			$('.divider, .talk').show()
-		} else {
-			$('.divider, .talk').hide()
-			$(`.date_${selectedDay}`).show()
-		}
-	})
-	$(document).on('click', '.talk-speakers li', function() {
-		const thisSpeaker = $(this).attr('id').replace(/^talk-/, '')
-		// console.log(thisSpeaker)
-		if ($(`#talk-${thisSpeaker}`).hasClass('selected')) {
-			$(`#talk-${thisSpeaker}`).removeClass('selected')
-			$(`#bio-${thisSpeaker}`).hide()
-		} else {
-			$(`#talk-${thisSpeaker}`).addClass('selected')
-			$(`#bio-${thisSpeaker}`).show()
-		}
-	})
-	$(document).on('click', '.section-selector li', function() {
-		const thisSection = $(this).attr('id')
-		// console.log(`thisSection: ${thisSection}`)
-		switch(thisSection) {
-			case 'talks-selector':
-				$('#talks-container').show()
-				$('#speakers-container').hide()
+  $(document).on('click', '.menu-icon', function() {
+    // $(this).hasClass('change')
+    const isOpen = $(this).hasClass('is-open')
+    console.log('Open/Close mobile menu')
+    if (isOpen) {
+      $('#sidebar-container').slideUp(100)
+      $(this).removeClass('is-open')
+    } else {
+      $('#sidebar-container').slideDown(100)
+      $(this).addClass('is-open')
+    }
+  })
+  $(document).on('click', '.days_filter li', function() {
+    // console.log($(this).attr('id'))
+    const selectedDay = $(this).attr('id').replace(/day_filter_/, '')
+    if (selectedDay.match(/all/)) {
+      $('.divider, .talk').show()
+    } else {
+      $('.divider, .talk').hide()
+      $(`.date_${selectedDay}`).show()
+    }
+  })
+  $(document).on('click', '.talk-speakers li', function() {
+    const thisSpeaker = $(this).attr('id').replace(/^talk-/, '')
+    // console.log(thisSpeaker)
+    if ($(`#talk-${thisSpeaker}`).hasClass('selected')) {
+      $(`#talk-${thisSpeaker}`).removeClass('selected')
+      $(`#bio-${thisSpeaker}`).hide()
+    } else {
+      $(`#talk-${thisSpeaker}`).addClass('selected')
+      $(`#bio-${thisSpeaker}`).show()
+    }
+  })
+  $(document).on('click', '.section-selector li', function() {
+    const thisSection = $(this).attr('id')
+    // console.log(`thisSection: ${thisSection}`)
+    switch(thisSection) {
+      case 'talks-selector':
+        $('#talks-container').show()
+        $('#speakers-container').hide()
         $('#talk-filters').show()
         $('#speaker-filters').hide()
-  			break
-			case 'speakers-selector':
-				$('#talks-container').hide()
-				$('#speakers-container').show()
+        break
+      case 'speakers-selector':
+        $('#talks-container').hide()
+        $('#speakers-container').show()
         $('#talk-filters').hide()
         $('#speaker-filters').show()
-  			break
+        break
       default:
         console.error('Whaaaacha doing?')
         break
-		}
-	})
+    }
+  })
   $(document).on('click', '.expand-collapse li', function() {
     const thisId = $(this).attr('id')
     const isCollapsed = $(this).hasClass('collapsed')
@@ -67,78 +79,78 @@ jQuery(document).ready(function($) {
         break
     }
   })
-	$(document).on('click', '.talk-title', function() {
-		const thisTalk = $(this).parent().parent().attr('id')
-		// console.log(`thisTalk: ${thisTalk}`)
+  $(document).on('click', '.talk-title', function() {
+    const thisTalk = $(this).parent().parent().attr('id')
+    // console.log(`thisTalk: ${thisTalk}`)
 
-		if ($(this).hasClass('selected')) {
-			// Talk open... close it
-			$(this).removeClass('selected')
-			$(`#${thisTalk} .talk-details`).hide()
-			$(`#${thisTalk}`).css('background-color', 'transparent')
-			$(`#${thisTalk} .talk-info`).removeClass('open')
-			$(`#${thisTalk} .talk-location`).show()
-		} else {
-			// Talk closed... open it
-			$(this).addClass('selected')
-			$(`#${thisTalk} .talk-details`).show()
-			$(`#${thisTalk}`).css('background-color', '#444')
-			$(`#${thisTalk} .talk-info`).addClass('open')
-			$(`#${thisTalk} .talk-location`).hide()
-		}
-	})
-	let days = {}
+    if ($(this).hasClass('selected')) {
+      // Talk open... close it
+      $(this).removeClass('selected')
+      $(`#${thisTalk} .talk-details`).hide()
+      $(`#${thisTalk}`).css('background-color', 'transparent')
+      $(`#${thisTalk} .talk-info`).removeClass('open')
+      $(`#${thisTalk} .talk-location`).show()
+    } else {
+      // Talk closed... open it
+      $(this).addClass('selected')
+      $(`#${thisTalk} .talk-details`).show()
+      $(`#${thisTalk}`).css('background-color', '#444')
+      $(`#${thisTalk} .talk-info`).addClass('open')
+      $(`#${thisTalk} .talk-location`).hide()
+    }
+  })
+  let days = {}
   let letters = []
   let countTalks = {}
-	if (window.conf) {
-		const conference = JSON.parse(window.conf)
-		// Build talks section
-		const conferenceSorted = sortByDate(conference)
-		// console.log(conference)
-		let lastTime = ''
-		let lastDate = ''
-		for (const talk in conferenceSorted) {
-			const thisTalk = conference.talks[conferenceSorted[talk]]
+  if (window.conf) {
+    const conference = JSON.parse(window.conf)
+    // Build talks section
+    const conferenceSorted = sortByDate(conference)
+    // console.log(conference)
+    let lastTime = ''
+    let lastDate = ''
+    for (const talk in conferenceSorted) {
+      const thisTalk = conference.talks[conferenceSorted[talk]]
 
       thisTalk.speakers.forEach(function (speakerId) {
         if (!countTalks.hasOwnProperty(speakerId)) countTalks[speakerId] = []
         countTalks[speakerId].push(`${thisTalk.title} - ${thisTalk.fullDate}`)
       })
 
-			// Display only unique dates
-			const thisDate = thisTalk.date.replace(/2018\.0*/, '').replace(/\./, '/')
-			let dateDivider = ''
-			if (thisDate !== lastDate) {
-				dateDivider = `<div class="divider date_divider date_${classify(thisDate)}">${thisTalk.day} ${thisDate}<span class="toTop"><a href="#">Back to top</a></span></div>`
-				days[thisDate] = thisTalk.day
-			}
-			lastDate = thisDate
+      // Display only unique dates
+      const thisDate = thisTalk.date.replace(/2018\.0*/, '').replace(/\./, '/')
+      let dateDivider = ''
+      if (thisDate !== lastDate) {
+        dateDivider = `<div class="divider date_divider date_${classify(thisDate)}">${thisTalk.day} ${thisDate}<span class="toTop"><a href="#">Back to top</a></span></div>`
+        days[thisDate] = thisTalk.day
+      }
+      lastDate = thisDate
 
-			// Display only unique times
-			const thisTime = thisTalk.start
-			let timeDivider = ''
-			if (thisTime !== lastTime) timeDivider = `<div class="divider time_divider date_${classify(thisDate)} time_${classify(thisTime)}">${thisTime}</div>`
-			lastTime = thisTime
-			let category = (thisTalk.tags.length ? `<div class="details-categories"><strong>Categories:</strong> ${thisTalk.tags.join(', ')}</div>` : '')
-			// Output
-			$('#talks-container').append(`
-				${dateDivider}
-				${timeDivider}
-				<div class="talk date_${classify(thisDate)} time_${classify(thisTime)}" id="${idify(talk)}">
-					<div class="talk-location">${clearWorkaround(thisTalk.location)}</div>
-					<div class="talk-info">
-						<div class="talk-title">${thisTalk.title}</div>
-					</div>
-					<div class="talk-details">
-						<div class="details-location"><strong>Location:</strong> ${clearWorkaround(thisTalk.location)}</div>
-						${category}
-						<div class="details-duration"><strong>Duration:</strong> ${thisTalk.duration} minutes</div>
-						<ul class="talk-speakers">${getSpeakerInfo(thisTalk.speakers, conference)}</ul>
-						<div class="talk-abstract">${thisTalk.abstract}</div>
-					</div>
-				</div>
-			`)
-		}
+      // Display only unique times
+      const thisTime = thisTalk.start
+      let timeDivider = ''
+      if (thisTime !== lastTime) timeDivider = `<div class="divider time_divider date_${classify(thisDate)} time_${classify(thisTime)}">${thisTime}</div>`
+      lastTime = thisTime
+      let category = (thisTalk.tags.length ? `<div class="details-categories"><strong>Categories:</strong> ${thisTalk.tags.join(', ')}</div>` : '')
+      // Output
+      $('#talks-container').append(`
+        ${dateDivider}
+        ${timeDivider}
+        <div class="talk date_${classify(thisDate)} time_${classify(thisTime)}" id="${idify(talk)}">
+          <div class="talk-location">${clearWorkaround(thisTalk.location)}</div>
+          <div class="talk-info">
+            <div class="talk-title">${thisTalk.title}</div>
+          </div>
+          <div class="talk-details">
+            <div class="details-location"><strong>Location:</strong> ${clearWorkaround(thisTalk.location)}</div>
+            ${category}
+            <div class="details-duration"><strong>Duration:</strong> ${thisTalk.duration} minutes</div>
+            <ul class="talk-speakers">${getSpeakerInfo(thisTalk.speakers, conference)}</ul>
+            <div class="talk-abstract">${thisTalk.abstract}</div>
+          </div>
+        </div>
+      `)
+    }
 
     // console.log(JSON.stringify(countTalks, null, 2))
 
@@ -167,18 +179,18 @@ jQuery(document).ready(function($) {
       `)
       trig = false
     }
-	} else { 
-		console.log('could not load conference')
-	}
-	drawFilters(days, window.lastUpdated, letters)
+  } else { 
+    console.log('could not load conference')
+  }
+  drawFilters(days, window.lastUpdated, letters)
 })
 function drawFilters(days, lastUpdated, letters) {
-	// console.log(`days: ${JSON.stringify(days, null, 2)}`)
-	let dayList = ''
-	for (let date in days) {
-		dayList += `<li id="day_filter_${classify(date)}">${days[date]} ${date}</li>`
-	}
-	$('#sidebar').append(`
+  // console.log(`days: ${JSON.stringify(days, null, 2)}`)
+  let dayList = ''
+  for (let date in days) {
+    dayList += `<li id="day_filter_${classify(date)}">${days[date]} ${date}</li>`
+  }
+  $('#sidebar-container').append(`
     <h3>Sections</h3>
 
     <ul class="section-selector">
@@ -187,11 +199,11 @@ function drawFilters(days, lastUpdated, letters) {
     </ul>
     <span id="talk-filters">
       <hr />
-  		<h3>Talk Filters</h3>
-  		<ul class="days_filter">
-  			<li id="day_filter_all">all</li>
-  			${dayList}
-  		</ul>
+      <h3>Talk Filters</h3>
+      <ul class="days_filter">
+        <li id="day_filter_all">all</li>
+        ${dayList}
+      </ul>
       <hr />
       <h3>Expand / Collapse</h3>
       <ul class="expand-collapse">
@@ -205,45 +217,45 @@ function drawFilters(days, lastUpdated, letters) {
       <h3>Jump To</h3>
       <ul class="speaker-jump">${letters.join('\n')}</ul>
     </span>
-		<hr />
-		<div class="last-updated">Last Updated: ${lastUpdated}</div>
-		`
-	)	
+    <hr />
+    <div class="last-updated">Last Updated: ${lastUpdated}</div>
+    `
+  )  
 }
 function getSpeakerInfo(speakerIds, conference) {
-	// console.log(speakerIds)
-	// return speakerIds.join(' ')
-	let ret = ''
-	speakerIds.forEach(function(speakerId) {
-		const thisSpeaker = conference.speakers[speakerId]
-		// console.log(speakerId)
-		ret += `<li id="talk-${speakerId}">
-			<strong>${thisSpeaker.name}</strong>
-			${thisSpeaker.title}
-			<div class="speaker-bio" id="bio-${speakerId}">${thisSpeaker.bio}</div>
-		</li>`
-	})
-	return ret
+  // console.log(speakerIds)
+  // return speakerIds.join(' ')
+  let ret = ''
+  speakerIds.forEach(function(speakerId) {
+    const thisSpeaker = conference.speakers[speakerId]
+    // console.log(speakerId)
+    ret += `<li id="talk-${speakerId}">
+      <strong>${thisSpeaker.name}</strong>
+      ${thisSpeaker.title}
+      <div class="speaker-bio" id="bio-${speakerId}">${thisSpeaker.bio}</div>
+    </li>`
+  })
+  return ret
 }
 function sortByDate(conference) {
-	let sorted = {}
-	let unsorted = {}
-	for (const talk in conference.talks) {
-		const thisTalk = conference.talks[talk]
-		const id = `${thisTalk.date}.${thisTalk.start.replace(/:/g, '.')}.${slug(thisTalk.location)}-${talk}` 
-		unsorted[id] = talk
-	}
-	Object.keys(unsorted).sort().forEach(function(id) { sorted[id] = unsorted[id] })
-	return sorted
+  let sorted = {}
+  let unsorted = {}
+  for (const talk in conference.talks) {
+    const thisTalk = conference.talks[talk]
+    const id = `${thisTalk.date}.${thisTalk.start.replace(/:/g, '.')}.${slug(thisTalk.location)}-${talk}` 
+    unsorted[id] = talk
+  }
+  Object.keys(unsorted).sort().forEach(function(id) { sorted[id] = unsorted[id] })
+  return sorted
 }
 function classify(str) {
-	return str.replace(/\./g, '').replace(/^.*\//, '').replace(/\:/g, '')
+  return str.replace(/\./g, '').replace(/^.*\//, '').replace(/\:/g, '')
 }
 function idify(str) {
-	return str.replace(/\./g, '-')
+  return str.replace(/\./g, '-')
 }
 function clearWorkaround(str) {
-	return str.replace(/^.*in 101/, '101').replace(/^.*in Octavius/, 'Octavius')
+  return str.replace(/^.*in 101/, '101').replace(/^.*in Octavius/, 'Octavius')
 }
 function slug(name) {
   return name.toLowerCase()
